6909b1ede3d36a48c506e8e9104a33eb48fb9d9b
Ciro <cironidas@gmail.com>
2024-02-16 21:22:39 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,14 +5,18 @@ from pinecone import Pinecone, ServerlessSpec
 import os
 from io import BytesIO
 
+ 
+
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY_V2"]
 PINECONE_ENV = st.secrets["PINECONE_ENV"]
 PINECONE_INDEX_NAME = st.secrets["PINECONE_INDEX_NAME"]
+
 
 api_key = st.secrets["OPENAI_API_KEY"]
 api_base = st.secrets["OPENAI_API_BASE"]
 api_version = st.secrets["OPENAI_API_VERSION"]
 api_type = st.secrets["OPENAI_API_TYPE"]
+
 
 openai.api_key = api_key
 openai.api_base = api_base
@@ -22,37 +26,43 @@ openai.api_type = api_type
 EMBEDDING_MODEL = "text-embedding-ada-002"
 GPT_EMBEDDING_ENGINE = 'mondongodb'
 DIMENSION = 1536
+#GPT_MODEL = 'gpt-3.5-turbo-16k'
+#GPT_CHAT_ENGINE = "gepeto"
 GPT_MODEL = 'gpt-4'
 GPT_CHAT_ENGINE = "dictador"
+
+
+  
+
 
 st.title("Chatbot")
 
 is_pdf_chatbot = st.checkbox("PDF chatbot")
-
 #uploaded_file = st.sidebar.file_uploader("Sube tu archivo PDF", type="pdf")
 
+#if uploaded_file is not None:
+#  pdf_bytes = uploaded_file.read()
+#  pdf_file = BytesIO(pdf_bytes)
+#  #docsearch = pdf_gpt.process_pdf(pdf_file,api_key,PINECONE_API_KEY,PINECONE_ENV,PINECONE_INDEX_NAME, DIMENSION)
+#  st.success("¡Archivo PDF procesado exitosamente!")
+#  pdf_file.close()
 
-    
+
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Hola, soy ChatGPT, ¿En qué puedo ayudarte?"}]
-
-try:
-    for msg in st.session_state["messages"]:
-      st.chat_message(msg["role"]).write(msg["content"])
-
-except KeyError:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Hola, soy ChatGPT, ¿En qué puedo ayudarte?"}]
-    for msg in st.session_state["messages"]:
-      st.chat_message(msg["role"]).write(msg["content"])
+  st.session_state["messages"] = [{"role": "assistant", "content": "Hola, soy ChatGPT, ¿En qué puedo ayudarte?"}]
+  
+for msg in st.session_state["messages"]:
+  st.chat_message(msg["role"]).write(msg["content"])
 
 if user_input := st.chat_input():
-    st.session_state["messages"].append({"role": "user", "content": user_input})
-    st.chat_message("user").write(user_input)
-    response = openai.ChatCompletion.create(model=GPT_MODEL,
-                                            messages=st.session_state["messages"],
-                                            engine=GPT_CHAT_ENGINE,
-                                            max_tokens=DIMENSION
-                                            )
-    responseMessage = response['choices'][0]['message']['content']
-    st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
-    st.chat_message("assistant").write(responseMessage)
+  st.session_state["messages"].append({"role": "user", "content": user_input})
+  st.chat_message("user").write(user_input)
+  response = openai.ChatCompletion.create(
+      model=GPT_MODEL,
+      messages=st.session_state["messages"],
+      engine=GPT_CHAT_ENGINE,
+      max_tokens=DIMENSION
+  )
+  responseMessage = response['choices'][0]['message']['content']
+  st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
+  st.chat_message("assistant").write(responseMessage)
